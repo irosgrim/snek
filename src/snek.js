1fda39148679b35130fc12f8cc70f1b7cf376ada
ioan rosgrim <ioan.rosgrim@ingka.ikea.com>
2022-12-15 23:24:58 +0100

    add mobile joystick

--- src/snek.js
+++ src/snek.js
@@ -1,26 +1,3 @@
-const up = document.getElementById("up");
-const down = document.getElementById("down");
-const left = document.getElementById("left");
-const right = document.getElementById("right");
-const space = document.getElementById("space");
-
-
-up.addEventListener("click", (e) => {
-    window.dispatchEvent(new KeyboardEvent("keydown", {key: "ArrowUp"}))
-});
-down.addEventListener("click", (e) => {
-    window.dispatchEvent(new KeyboardEvent("keydown", {key: "ArrowDown"}))
-});
-left.addEventListener("click", (e) => {
-    window.dispatchEvent(new KeyboardEvent("keydown", {key: "ArrowLeft"}))
-});
-right.addEventListener("click", (e) => {
-    window.dispatchEvent(new KeyboardEvent("keydown", {key: "ArrowRight"}))
-});
-space.addEventListener("click", (e) => {
-    window.dispatchEvent(new KeyboardEvent("keydown", {key: " "}))
-});
-
 const distance = (pointA, pointB) => {
     return Math.floor(Math.sqrt((pointA.x - pointB.x) ** 2 + (pointA.y - pointB.y) ** 2));
 }
