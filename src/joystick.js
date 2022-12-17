242e908691005c5325f17c8a3a893ae4be83ae54
ioan rosgrim <ioan.rosgrim@ingka.ikea.com>
2022-12-17 13:36:49 +0100

    remove stuff

--- src/joystick.js
+++ src/joystick.js
@@ -1,33 +1,25 @@
 let joystick;
 
-let timeoutCreate;
-function createThrottle (evt) {
-  clearTimeout(timeoutCreate);
-  timeoutCreate = setTimeout(() => {
-    createNipple(evt);
-  }, 100);
-}
-
 createNipple('static');
 
 function bindNipple() {
   joystick.on('dir:up plain:up dir:left plain:left dir:down plain:down dir:right plain:right',
-        function(evt, data) {
-          const angle = data.direction.angle;
-          if (angle === "up") {
-            window.dispatchEvent(new KeyboardEvent("keydown", {key: "ArrowUp"}))
-          }
-          if (angle === "down") {
-            window.dispatchEvent(new KeyboardEvent("keydown", {key: "ArrowDown"}))
-          }
-          if (angle === "left") {
-            window.dispatchEvent(new KeyboardEvent("keydown", {key: "ArrowLeft"}))
-          }
-          if (angle === "right") {
-            window.dispatchEvent(new KeyboardEvent("keydown", {key: "ArrowRight"}))
-          }
-        }
-       );
+    (evt, data) => {
+      const angle = data.direction.angle;
+      if (angle === "up") {
+        window.dispatchEvent(new KeyboardEvent("keydown", {key: "ArrowUp"}))
+      }
+      if (angle === "down") {
+        window.dispatchEvent(new KeyboardEvent("keydown", {key: "ArrowDown"}))
+      }
+      if (angle === "left") {
+        window.dispatchEvent(new KeyboardEvent("keydown", {key: "ArrowLeft"}))
+      }
+      if (angle === "right") {
+        window.dispatchEvent(new KeyboardEvent("keydown", {key: "ArrowRight"}))
+      }
+    }
+    );
 }
 
 function createNipple(type) {
