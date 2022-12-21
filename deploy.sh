bbf078560729b4e8372c8bdc12b5df74d7df66ca
ioan rosgrim <ioan.rosgrim@ingka.ikea.com>
2022-12-21 20:21:13 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,18 +6,12 @@ set -e
 # build
 npm run build
 
-# navigate into the build output directory
-cd dist
+git add dist -f
 
 # git init
 # git checkout -b main
 git add .
 git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:irosgrim/snek.git main:gh-pages
+git subtree push --prefix dist origin gh-pages
 
 cd -
